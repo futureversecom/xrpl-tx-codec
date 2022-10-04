d28d2292f7760635d53603096ffdf94cd907f1d3
surangap <surangatco@gmail.com>
2022-10-04 11:44:14 +0530

    Add STArrayType

--- codec/src/types.rs
+++ codec/src/types.rs
@@ -112,6 +112,19 @@ impl BinarySerialize for SignerEntryType {
     }
 }
 
+#[derive(Debug)]
+pub struct STArrayType<T>(pub Vec<T>);
+impl<T: BinarySerialize> BinarySerialize for STArrayType<T> {
+    fn binary_serialize_to(&self, buf: &mut Vec<u8>, _for_signing: bool) {
+        // no order, serialize the way it is. Ref -> https://xrpl.org/serialization.html#array-fields
+        for item in &self.0 {
+            item.binary_serialize_to(buf, _for_signing);
+        }
+        // Append the array end here. Ref -> https://xrpl.org/serialization.html#array-fields
+        buf.push(0xf1);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -136,6 +149,39 @@ mod tests {
         expected_buf.extend_from_slice(&[account_field_vl]);
         expected_buf.extend_from_slice( &[1_u8; 20]);
         expected_buf.extend_from_slice(&[st_object_end]);
+
+        assert_eq!(buf, expected_buf);
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_STArrayType() {
+        // use SignerEntry
+        let mut signer_entries  = Vec::<SignerEntry>::default();
+        for i in 1..=2 {
+            signer_entries.push(SignerEntry(SignerEntryType(Account(AccountIdType([i as u8; 20])),
+                                            SignerWeight(UInt16Type(i as u16)))));
+        }
+        let st_array_type = STArrayType(signer_entries);
+
+        let buf = st_array_type.binary_serialize(true);
+        let signer_entry_field_id: u8 = 0xEB; // Typecode(14) | FieldCode(11) = 0xEB
+        let account_field_id: u8 = 0x81; // Typecode(8) | Fieldcode(1) = 0x81(129)
+        let signer_weight_field_id: u8 = 0x13; // Typecode(1) | Fieldcode(3) = 0x13(19)
+        let account_field_vl: u8 = 0x14; // https://xrpl.org/serialization.html#accountid-fields
+        let st_object_end: u8 = 0xe1; // https://xrpl.org/serialization.html#object-fields
+
+        // let's construct the expected buffer -> https://xrpl.org/serialization.html#array-fields
+        let mut expected_buf = Vec::<u8>::default();
+        for i in 1..=2 {
+            expected_buf.extend_from_slice(&[signer_entry_field_id]); // SignerEntry field ID
+            expected_buf.extend_from_slice(&[signer_weight_field_id]); // SignerWeight comes first in the canonical order
+            expected_buf.extend_from_slice(&(i as u16).to_be_bytes());
+            expected_buf.extend_from_slice(&[account_field_id]);
+            expected_buf.extend_from_slice(&[account_field_vl]);
+            expected_buf.extend_from_slice(&[i as u8; 20]);
+            expected_buf.extend_from_slice(&[st_object_end]);
+        }
+        expected_buf.extend_from_slice(&[0xf1]); // STArray end 0xf1
 
         assert_eq!(buf, expected_buf);
     }
