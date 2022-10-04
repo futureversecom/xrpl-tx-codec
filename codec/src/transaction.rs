f6e8bd9b3be762345dfad0db201992ccb48c727c
surangap <surangatco@gmail.com>
2022-10-04 13:03:44 +0530

    implement SignerListSet tx, add tests.

--- codec/src/transaction.rs
+++ codec/src/transaction.rs
@@ -7,6 +7,7 @@ use crate::{
     types::{AccountIdType, AmountType, BlobType, UInt32Type},
     Vec,
 };
+use crate::types::{STArrayType};
 
 /*
    Tx Common Fields
@@ -136,9 +137,49 @@ pub struct SignerListSet {
     txn_signature: TxnSignature,
 }
 
+impl SignerListSet {
+    /// Create a new XRP SignerListSet transaction
+    ///
+    /// Applies the global signing flags (see https://xrpl.org/transaction-common-fields.html#global-flags)
+    ///
+    /// - `account` the sender's address
+    /// - `fee` the max XRP fee in drops
+    /// - `signer_quorum` signer quorum required
+    /// - `signer_entries` signer entries which can participate in multi signing
+    /// - `signing_pub_key` public key of `account`
+    pub fn new(
+        account: [u8; 20],
+        fee: u64,
+        signer_quorum: u32,
+        signer_entries: Vec::<SignerEntry>,
+        signing_pub_key: Option<[u8; 33]>,
+    ) -> Self {
+        Self {
+            account: Account(AccountIdType(account)),
+            transaction_type: TransactionTypeCode::SignerListSet.into(),
+            fee: Fee(AmountType(fee)),
+            // https://xrpl.org/transaction-common-fields.html#global-flags
+            flags: Flags(UInt32Type(0x8000_0000_u32)),
+            /// payment only
+            signer_quorum: SignerQuorum(UInt32Type(signer_quorum)),
+            signer_entries: SignerEntries(STArrayType(signer_entries)),
+            signing_pub_key: signing_pub_key
+                .map(|pk| SigningPubKey(BlobType(pk.to_vec())))
+                .unwrap_or_default(),
+            txn_signature: Default::default(),
+        }
+    }
+    /// Attach a signature to the transaction
+    pub fn attach_signature(&mut self, signature: [u8; 65]) {
+        self.txn_signature = TxnSignature(BlobType(signature.to_vec()));
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{CodecToFields, Payment};
+    use crate::field::{Account, SignerEntry, SignerWeight};
+    use crate::types::{AccountIdType, SignerEntryType, UInt16Type};
+    use super::{CodecToFields, Payment, SignerListSet};
 
     #[test]
     #[allow(non_snake_case)]
@@ -165,6 +206,40 @@ mod tests {
                         f1.type_code() < f2.type_code()
                             || f1.type_code() == f2.type_code()
                                 && f1.field_code() <= f2.field_code()
+                    );
+                }
+                _ => continue,
+            }
+        }
+    }
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_SignerListSet_canonical_field_order() {
+        let account = [1_u8; 20];
+        let fee = 1_000; // 1000 drops
+        let signing_pub_key = [1_u8; 33];
+        let signer_quorum  = 3_u32;
+        let mut signer_entries = Vec::<SignerEntry>::default();
+        signer_entries.push(SignerEntry(SignerEntryType(Account(AccountIdType([1_u8; 20])),
+                                                        SignerWeight(UInt16Type(1_u16)))));
+        signer_entries.push(SignerEntry(SignerEntryType(Account(AccountIdType([2_u8; 20])),
+                                                        SignerWeight(UInt16Type(2_u16)))));
+
+        let signer_list_set = SignerListSet::new(
+            account,
+            fee,
+            signer_quorum,
+            signer_entries,
+            Some(signing_pub_key),
+        );
+
+        for chunk in signer_list_set.to_canonical_fields().chunks(2) {
+            match chunk {
+                &[f1, f2] => {
+                    assert!(
+                        f1.type_code() < f2.type_code()
+                            || f1.type_code() == f2.type_code()
+                            && f1.field_code() <= f2.field_code()
                     );
                 }
                 _ => continue,
