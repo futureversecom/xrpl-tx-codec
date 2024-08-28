b14ca9f03503d702ea958194949d54a0297f278e
surangap <19677661+surangap@users.noreply.github.com>
2024-08-28 17:02:41 +1200

    Support token amounts in AmountType.

--- codec/src/types.rs
+++ codec/src/types.rs
@@ -1,10 +1,13 @@
 //! XRPL codec primitive types
 
+use crate::error::Error;
 use crate::{
     field::{Account, SignerWeight},
     traits::BinarySerialize,
     Vec,
 };
+use alloc::format;
+use alloc::string::ToString;
 
 pub const ACCOUNT_ID_TYPE_CODE: u16 = 8;
 
@@ -105,14 +108,156 @@ impl BinarySerialize for CurrencyCode {
     }
 }
 
-/// Current
-///ly supporting native XRP amounts only
+/// The value of Issued amount, ref - https://xrpl.org/docs/references/protocol/data-types/currency-formats#string-numbers
 #[derive(Debug, Clone)]
-pub struct AmountType(pub u64);
+pub struct IssuedValue {
+    pub mantissa: i64,
+    pub exponent: i8,
+}
+
+impl IssuedValue {
+    /// Creates value from given mantissa and exponent. The created value will be normalized
+    /// according to https://xrpl.org/docs/references/protocol/binary-format#token-amount-format. If the value
+    /// cannot be represented, an error is returned.
+    pub fn from_mantissa_exponent(mantissa: i64, exponent: i8) -> Result<Self, Error> {
+        Self { mantissa, exponent }.normalize()
+    }
+
+    pub fn zero() -> Self {
+        Self {
+            mantissa: 0,
+            exponent: 0,
+        }
+    }
+
+    /// Normalizes value into the ranges specified in https://xrpl.org/docs/references/protocol/binary-format#token-amount-format
+    fn normalize(self) -> Result<Self, Error> {
+        // rippled implementation: https://github.com/seelabs/rippled/blob/cecc0ad75849a1d50cc573188ad301ca65519a5b/src/ripple/protocol/impl/IOUAmount.cpp#L38
+        const MANTISSA_MIN: i64 = 1000000000000000;
+        const MANTISSA_MAX: i64 = 9999999999999999;
+        const EXPONENT_MIN: i8 = -96;
+        const EXPONENT_MAX: i8 = 80;
+
+        let mut exponent = self.exponent;
+        let (mut mantissa, negative) = match self.mantissa {
+            0 => {
+                return Ok(Self::zero());
+            }
+            1.. => (self.mantissa, false),
+            ..=-1 => (
+                self.mantissa.checked_neg().ok_or_else(|| {
+                    Error::OutOfRange("Specified mantissa cannot be i64::MIN".to_string())
+                })?,
+                true,
+            ),
+        };
+
+        while mantissa < MANTISSA_MIN && exponent > EXPONENT_MIN {
+            mantissa *= 10;
+            exponent -= 1;
+        }
+
+        while mantissa > MANTISSA_MAX && exponent < EXPONENT_MAX {
+            mantissa /= 10;
+            exponent += 1;
+        }
+
+        if mantissa > MANTISSA_MAX || exponent > EXPONENT_MAX {
+            return Err(Error::OutOfRange(format!(
+                "Issued value too big to be normalized: {:?}",
+                self
+            )));
+        }
+
+        if mantissa < MANTISSA_MIN || exponent < EXPONENT_MIN {
+            return Ok(Self::zero());
+        }
+
+        if negative {
+            mantissa = -mantissa;
+        }
+
+        Ok(Self { mantissa, exponent })
+    }
+}
+
+impl BinarySerialize for IssuedValue {
+    fn binary_serialize_to(&self, buf: &mut Vec<u8>, for_signing: bool) {
+        // https://xrpl.org/docs/references/protocol/binary-format#token-amount-format
+        const ISSUED_MASK: u64 = 0x8000000000000000;
+        const POSITIVE_MASK: u64 = 0x4000000000000000;
+
+        let (mantissa, positive) = match self.mantissa {
+            0 => {
+                ISSUED_MASK.binary_serialize_to(buf, for_signing);
+                return;
+            }
+            1.. => (self.mantissa as u64, true),
+            ..=-1 => (-self.mantissa as u64, false),
+        };
+        let exponent = (self.exponent + 97) as u64;
+        let payload =
+            ISSUED_MASK | (if positive { POSITIVE_MASK } else { 0 }) | mantissa | (exponent << 54);
+        payload.binary_serialize_to(buf, for_signing);
+    }
+}
+
+/// Amount of issued token. ref - https://xrpl.org/docs/references/protocol/data-types/currency-formats#token-amounts,
+#[derive(Debug, Clone)]
+pub struct IssuedAmount {
+    // fields are private since it is validated when the IssuedAmount value is created
+    pub value: IssuedValue,
+    pub currency: CurrencyCode,
+    pub issuer: AccountIdType,
+}
+
+impl IssuedAmount {
+    pub fn from_issued_value(
+        value: IssuedValue,
+        currency: CurrencyCode,
+        issuer: AccountIdType,
+    ) -> Result<Self, Error> {
+        if currency.is_valid() {
+            return Err(Error::InvalidData(
+                "Issued amount cannot have invalid currency code".to_string(),
+            ));
+        }
+        Ok(Self {
+            value,
+            currency,
+            issuer,
+        })
+    }
+}
+
+impl BinarySerialize for IssuedAmount {
+    fn binary_serialize_to(&self, buf: &mut Vec<u8>, for_signing: bool) {
+        // https://xrpl.org/docs/references/protocol/binary-format#amount-fields
+        self.value.binary_serialize_to(buf, for_signing);
+        self.currency.binary_serialize_to(buf, for_signing);
+        self.issuer.binary_serialize_to(buf, for_signing);
+    }
+}
+
+/// Amount type, ref - https://xrpl.org/docs/references/protocol/data-types/currency-formats#specifying-currency-amounts
+#[derive(Debug, Clone)]
+pub enum AmountType {
+    Issued(IssuedAmount), // For tokens
+    Drops(u64),           // For XRP
+}
+
 impl BinarySerialize for AmountType {
-    fn binary_serialize_to(&self, buf: &mut Vec<u8>, _for_signing: bool) {
-        // https://xrpl.org/serialization.html#amount-fields
-        buf.extend_from_slice((self.0 | 0x4000000000000000).to_be_bytes().as_slice());
+    fn binary_serialize_to(&self, buf: &mut Vec<u8>, for_signing: bool) {
+        // https://xrpl.org/docs/references/protocol/binary-format#amount-fields
+        match self {
+            AmountType::Issued(issued_amount) => {
+                issued_amount.binary_serialize_to(buf, for_signing)
+            }
+            AmountType::Drops(drops_amount) => {
+                const POSITIVE_MASK: u64 = 0x4000000000000000;
+                buf.extend_from_slice((drops_amount | POSITIVE_MASK).to_be_bytes().as_slice());
+            }
+        }
     }
 }
 
